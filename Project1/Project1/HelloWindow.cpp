8ba965fcfdf7cfce931a9ebb2dbb184af49327f9
imjae <code@kakao.com>
2023-06-16 06:15:41 +0900

    glfw keyboard inpue mapping

--- Project1/Project1/HelloWindow.cpp
+++ Project1/Project1/HelloWindow.cpp
@@ -7,6 +7,12 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
 	glfwInit();
@@ -35,6 +41,8 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		processInput(window);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
