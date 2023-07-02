ce1c66284808670de815160e8dc37cce27ebac7a
imjae <code@kakao.com>
2023-07-03 05:51:59 +0900

    회전, 이동 행렬조합 순서 바꿔보기

--- chap02/textures/textures/main.cpp
+++ chap02/textures/textures/main.cpp
@@ -155,12 +155,6 @@ int main()
     // or set it via the texture class
     // ourShader.setInt("texture2", 1);
 
-    glm::mat4 trans(1.0f);
-    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
     float ratioValue = 0.0;
     // render loop
     // -----------
@@ -199,6 +193,12 @@ int main()
         }
         ourShader.setFloat("ratio", ratioValue);
         
+        glm::mat4 trans(1.0f);
+
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
