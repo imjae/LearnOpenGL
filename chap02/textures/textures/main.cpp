b528267a2105c5c40f1f5654a6394f497688136e
imjae <code@kakao.com>
2023-07-06 05:47:36 +0900

    특정 육면체만 회전시켜보기

--- chap02/textures/textures/main.cpp
+++ chap02/textures/textures/main.cpp
@@ -215,7 +215,7 @@ int main()
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.01f, 100.0f);
 
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
@@ -232,8 +232,14 @@ int main()
         {
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[index]);
-            float angle = 20.0f * index;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+            float resultAngle = 20.0f * index;
+            if (index % 3 == 1)
+            {
+                resultAngle = (float)20 * glfwGetTime();
+            }
+
+            model = glm::rotate(model, glm::radians(resultAngle), glm::vec3(1.0f, 0.3f, 0.5f));
             glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
                 
             glDrawArrays(GL_TRIANGLES, 0, 36);
