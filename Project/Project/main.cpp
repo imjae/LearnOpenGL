3516f300714de98f341d8af644138a0008cd397e
임재경 <jakeim11@zempot.com>
2023-07-05 14:50:28 +0900

    Project GLFW/GLAD 셋팅

---
+++ Project/Project/main.cpp
@@ -0,0 +1,15 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	return 0;
+}
