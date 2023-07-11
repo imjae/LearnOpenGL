6d57ddf1156aaa9ef5c8ea72ca2bf8fd47f8ebfc
임재경 <jakeim11@zempot.com>
2023-07-11 21:07:42 +0900

    [R] 버텍스 쉐이더에서 컬러값 지정해보기

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -5,15 +5,18 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 "}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vertexColor;\n"
 "}\n\0";
 
 void processInput(GLFWwindow* window);
