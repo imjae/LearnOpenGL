1b7df82376e4eec83ac31ff9bd57b43cf6a07f53
imjae <code@kakao.com>
2023-07-16 15:28:27 +0900

    [Review] MyShader 클래스 추가

---
+++ Review/Review/MyShader.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<glad/glad.h>
+
+#include<string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class MyShader
+{
+public:
+    // program ID
+    unsigned int ID;
+
+    // 생성자는 shader를 읽고 생성합니다.
+    MyShader(const GLchar* vertexPath, const GLchar* fragmentPath);
+    // shader를 활성화하고 사용합니다.
+    void use();
+    // Uniform 유틸리티 함수들
+    void setBool(const std::string& name, bool value) const;
+    void setInt(const std::string& name, int value) const;
+    void setFloat(const std::string& name, float value) const;
+};
+
