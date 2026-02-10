962daee942694516f17baff8a5cc1842cf0b81da
imjae <code@kakao.com>
2026-02-11 05:09:06 +0900

    셰이더 문자오류 수정

--- Chapter01/Main.cpp
+++ Chapter01/Main.cpp
@@ -8,7 +8,7 @@ const char* vertexShaderSource =
 "layout(location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
-"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0)\n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
 
 const char* fragmentShaderSource =
@@ -16,7 +16,7 @@ const char* fragmentShaderSource =
 "vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f)\n"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
