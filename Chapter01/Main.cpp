df4d86fc86b1b50a4f27f165d5aad4c5da17f76a
imjae <code@kakao.com>
2026-02-11 13:32:02 +0900

    삼각형 그리기 실패

--- Chapter01/Main.cpp
+++ Chapter01/Main.cpp
@@ -76,12 +76,17 @@ int main()
 
 		unsigned int VBO;
 		glGenBuffers(1, &VBO);
+		/*glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);*/
+
+		/*glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);*/
+
+		unsigned int VAO;
+		glGenVertexArrays(1, &VAO);
+		glBindVertexArray(VAO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
 
@@ -122,6 +127,8 @@ int main()
 		}
 
 		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glDeleteShader(vertexShader);
 		glDeleteShader(fragmentShader);
