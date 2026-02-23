0ca9db77bf1b5c375f40a04d6ee7e87e7ec44dd2
imjae <code@kakao.com>
2026-02-23 13:32:15 +0900

    Uniform 타입 변수에 랜더스크립트에서 값 할당하기

--- Chapter01/Main.cpp
+++ Chapter01/Main.cpp
@@ -16,10 +16,10 @@ const char* vertexShaderSource =
 const char* fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 FragColor;\n"
-"in vec4 vertexColor;\n"
+"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = vertexColor;\n"
+"	FragColor = ourColor;\n"
 "}\0";
 
 const char* fragmentShaderSource2 =
@@ -196,6 +196,12 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
+
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
